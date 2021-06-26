aac9756f9762b8713f362af44047944d7e888e93
Kerbelyte <80693721+Kerbelyte@users.noreply.github.com>
2021-06-26 15:56:41 +0300

    add style and back button

--- index.php
+++ index.php
@@ -6,31 +6,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FileBrowser</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
 
-    <div class="BrowserTable">
-        <h1>Directory contents:</h1>
+    <div class="browser_table">
         <?php
-
         $rootDir = '/Applications/XAMPP/xamppfiles/htdocs';
         $diff = '';
         if (!empty($_GET['path'])) {
             $currentPath = $rootDir . '/' . $_GET['path'];
             $diff = str_replace($rootDir, '', $currentPath);
+            echo '<h1>Directory contents: ' . "$diff" . '</h1>';
         } else {
             $currentPath = $rootDir;
+            echo '<h1>Directory contents: ' . "$currentPath" . '</h1>';
         }
         $files = array_slice(scandir($currentPath), 2);
 
         echo '<table>
-            <tr>
+            <tr class="column_names">
             <td>Type</td>
             <td>Name</td>
             <td>Actions</td>
             </tr>';
-        for ($i = 0; $i < count($files); $i++) {
+        for ($i = 1; $i < count($files); $i++) {
             $name = $files[$i];
             if (is_dir($rootDir . $diff . '/' . $name)) {
                 $type = 'Directory';
@@ -42,22 +44,16 @@
             echo '<tr>
                         <td>' . $type . '</td>
                         <td>' . $name . '</td>
-                        <td>'. '#' . '</td>
+                        <td>' . '#' . '</td>
                     </tr>';
         }
 
         echo '</table>';
 
-        if (!isset($_COOKIE['counter'])) {
-            $cookie = 1;
-            setcookie('counter', $cookie);
-        } else {
-            $cookie = ++$_COOKIE['counter'];
-            setcookie('counter', $cookie);
-        }
-        echo 'You have viewed this page ' . $cookie . ' times' . "<br />";
+
 
         ?>
+        <button class="back_button" href="#" onclick="history.go(-1);">Back</button>
     </div>
 </body>
 
